96270b3ebd88fc7e7533dc1e2579ba6fe7a7beb0
Diego Pessoa <diego.pessoa@ifpb.edu.br>
2025-07-20 12:56:01 -0300

    adiciona exemplo de formulário

---
+++ exemplo/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, flash, session, redirect, url_for
+
+app = Flask(__name__)
+app.secret_key = 'uma string secreta'  # Necessário para flash e sessão
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    name = session.get('name')
+    if request.method == 'POST':
+        new_name = request.form.get('name')
+        if new_name:
+            if name and name != new_name:
+                flash('Parece que você mudou seu nome!')
+            session['name'] = new_name
+            return redirect(url_for('index'))
+        else:
+            flash('Por favor, preencha o nome.')
+    return render_template('index.html', name=session.get('name'))
+
+if __name__ == '__main__':
+    app.run(debug=True) 
